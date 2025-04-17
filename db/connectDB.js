f32dc68a6378db6b4a90ad9399bc6b96fe581184
Mohamed Nabeel <nabeelnbe200@gmail.com>
2025-04-17 09:11:39 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
 
     try{
-        const conn = await mongoose.connect('mongodb+srv://atlas-sample-dataset-load-680072fa59f31e20b0df48f3:<db_password>@cluster0.hytz1jh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{})
+        const conn = await mongoose.connect('mongodb://mohamed_nabeel:<db_password>@ac-8kwodvq-shard-00-00.hytz1jh.mongodb.net:27017,ac-8kwodvq-shard-00-01.hytz1jh.mongodb.net:27017,ac-8kwodvq-shard-00-02.hytz1jh.mongodb.net:27017/?replicaSet=atlas-10demg-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0',{})
         console.log(`mongoDB connected: ${conn.connection.host}`)
     }catch(error){
 
